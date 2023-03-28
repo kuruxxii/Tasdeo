4395081e5ea95a33c7e6ea374eabc891775ef405
qIIsheng <zzzihanlin@gmail.com>
2023-03-28 18:43:46 +0800

    update

--- src/pages/RemarkRecords.jsx
+++ src/pages/RemarkRecords.jsx
@@ -1,36 +1,27 @@
 import React from "react";
-import Remark from "../components/Remark";
+import RemarkRecord from "../components/RemarkRecord";
 import { MdCommentBank } from "react-icons/Md";
 import { Link } from "react-router-dom";
+import { requireAuth } from "../util";
 
-export default function HistoryRemarks() {
+export async function loader() {
+  await requireAuth();
+  return null;
+}
+
+export default function RemarkRecords() {
   return (
     <div className="w-full h-full overflow-auto relative">
       <p className="font-extrabold text-3xl w-1/2 text-center mx-auto my-6">
         History Remarks of <span className="text-bright">Taha Ahamed</span>
       </p>
-      <Link
-        to="/remark-form"
-        className="flex items-center fixed bottom-16 right-12">
+      <Link className="flex items-center fixed bottom-16 right-12">
         <MdCommentBank className="text-5xl mr-1" />
         <span className="text-2xl">Leave a Remark</span>
       </Link>
       <ol class="relative border-l border-bright w-3/5 mx-auto">
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
-        <Remark />
+        <RemarkRecord />
+        <RemarkRecord />
       </ol>
     </div>
   );
