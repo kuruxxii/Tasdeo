8e086bb40631f3cb96e92cb9ac281d0c14e6b4c0
qIIsheng <zzzihanlin@gmail.com>
2023-03-29 15:08:49 +0800

    doing history

--- src/components/RemarkRecord.jsx
+++ src/components/RemarkRecord.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function RemarkRecord() {
   return (
-    <li class="mb-10 ml-4">
-      <div class="absolute w-3 h-3 bg-bright rounded-full mt-1.5 -left-1.5 border border-bright"></div>
-      <time class="mb-1 text-sm font-normal leading-none">February 2022</time>
-      <h3 class="text-lg font-semibold">by Conor Brand</h3>
-      <p class="font-normal">
+    <li className="mb-10 ml-4">
+      <div className="absolute w-3 h-3 bg-bright rounded-full mt-1.5 -left-1.5 border border-bright"></div>
+      <time className="mb-1 text-sm font-normal leading-none">
+        February 2022
+      </time>
+      <h3 className="text-lg font-semibold">by Conor Brand</h3>
+      <p className="font-normal">
         All of the pages and components are first designed in Figma and we keep
         a parity between the two versions even as we update the project. Ell
         faiewfo sanv vasio nevvawe fwwvwein sf.
--- src/pages/RemarkRecords.jsx
+++ src/pages/RemarkRecords.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 import RemarkRecord from "../components/RemarkRecord";
-import { MdCommentBank } from "react-icons/Md";
-import { Link } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import { requireAuth } from "../util";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 export async function loader() {
   await requireAuth();
-  return null;
+  const allRemarks = [];
+  const allRemarksSnapshot = await getDocs(collection(db, "remarks"));
+  allRemarksSnapshot.forEach((doc) => {
+    allRemarks.push(doc.data());
+  });
+  return allRemarks;
 }
 
 export default function RemarkRecords() {
+  const allRemarks = useLoaderData();
+  const studentId = useParams().studentid;
+  const remarksOfThisStudent = allRemarks.filter(
+    (remark) => remark.studentId === studentId
+  );
+  remarksOfThisStudent.sort(
+    (a, b) => b.timestamp.seconds - a.timestamp.seconds
+  );
   return (
     <div className="w-full h-full overflow-auto relative">
       <p className="font-extrabold text-3xl w-1/2 text-center mx-auto my-6">
-        History Remarks of <span className="text-bright">Taha Ahamed</span>
+        History Remarks
       </p>
       <Link className="flex items-center fixed bottom-16 right-12">
-        <MdCommentBank className="text-5xl mr-1" />
         <span className="text-2xl">Leave a Remark</span>
       </Link>
-      <ol class="relative border-l border-bright w-3/5 mx-auto">
+      <ol className="relative border-l border-bright w-3/5 mx-auto">
         <RemarkRecord />
         <RemarkRecord />
       </ol>
