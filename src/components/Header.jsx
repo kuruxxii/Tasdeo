3e867c5bd0bb6ad878564951a5b2d74ad83b363c
qIIsheng <zzzihanlin@gmail.com>
2023-03-31 00:09:20 +0800

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { GoPerson } from "react-icons/go";
+import logoUrl from "../images/logo.png";
 
 export default function Header() {
   const activeStyles = {
@@ -11,7 +12,9 @@ export default function Header() {
 
   return (
     <header className="border h-28 flex justify-around items-center">
-      <Link to="/">#Tasdeo.O</Link>
+      <Link to="/">
+        <img src={logoUrl} alt="Tasdeo Logo" className="object-cover w-36" />
+      </Link>
       <nav className="flex justify-between items-center space-x-4">
         <NavLink
           to="overview"
