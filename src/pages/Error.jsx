95199687e7b1e1df99c3d01ba8321131718f7ec4
qIIsheng <zzzihanlin@gmail.com>
2023-04-28 13:35:01 +0800

    other error pages

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { useRouteError } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { ReactComponent as Road } from "../images/road.svg";
 
 export default function Error() {
   const error = useRouteError();
   console.log(error);
-  return <div>There's an Error</div>;
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center space-y-8">
+      <Road className="w-1/2 h-auto lg:w-1/4" />
+      <p className="lg:text-xl">
+        Oops! There's an error. Developer is on his way...
+      </p>
+      <button className="bg-[#d7bb5b] hover:bg-green-500 active:bg-green-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 px-4 py-2 rounded-full flex justify-center items-center w-1/2 lg:w-1/6">
+        <BiArrowBack className="mr-1" />
+        <Link to="/">GO BACK</Link>
+      </button>
+    </div>
+  );
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,19 +1,19 @@
-import { NavLink } from "react-router-dom";
+import React from "react";
+import { BiArrowBack } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { ReactComponent as Taken } from "../images/taken.svg";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Page not found!</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia alias
-        cupiditate ad nostrum doloribus iste tempora quisquam excepturi
-        repellat, fugit cumque dolore magni possimus inventore neque provident!
-        Sunt, quo eos?
+    <div className="w-full h-screen flex flex-col justify-center items-center space-y-8">
+      <Taken className="w-1/2 h-auto lg:w-1/4" />
+      <p className="lg:text-xl">
+        The page you are looking has been taken by aliens!
       </p>
-
-      <p>
-        Go to the <NavLink to="/dashboard">dashboard</NavLink>.
-      </p>
+      <button className="bg-[#d7bb5b] hover:bg-green-500 active:bg-green-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 px-4 py-2 rounded-full flex justify-center items-center w-1/2 lg:w-1/6">
+        <BiArrowBack className="mr-1" />
+        <Link to="/">GO BACK</Link>
+      </button>
     </div>
   );
 }
