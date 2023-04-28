eb822232011497c436519ab98bd8af0d57241af8
qIIsheng <zzzihanlin@gmail.com>
2023-04-28 14:08:31 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,10 +13,11 @@ export default function Home() {
           Teachers and students develop each other.
         </h1>
         <p className="w-4/5 lg:w-1/2 text-base lg:text-lg text-white text-center lg:leading-loose">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas
-          porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies,
-          purus lectus malesuada libero, sit amet commodo magna eros quis urna.
-          Nunc viverra imperdiet enim. Fusce est.
+          Welcome to Tasdeo, an online platform where professors can leave
+          remarks for students and view history remarks given by other
+          professors. Our mission is to provide evaluation criteria other than
+          academic records to help teachers and students connect, share ideas,
+          and inspire each other to reach their full potential.
         </p>
         <button className="bg-[#d7bb5b] hover:bg-green-500 active:bg-green-600 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 px-8 py-2 lg:px-12 lg:py-4 rounded-full text-base lg:text-xl flex items-center">
           <Link to="sign-in">EXPLORE</Link>
